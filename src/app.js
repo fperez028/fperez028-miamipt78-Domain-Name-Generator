e2adee5748c8a2c04478d5083eed07797ba301db
fperez028 <21010185+fperez028@users.noreply.github.com>
2025-03-28 19:31:09 -0400

    project update 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-import "bootstrap";
-import "./style.css";
+// Domain name generator
+// Using multiple arrays
+// We want to output every possible combination of the words in the arrays
 
+// Output container in the HTML for DOM Manipulation
+let outputDiv = document.getElementById("output");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// 1. Create a set of arrays to make our permutations
+let pronoun = ['the','our','my'];
+let adj = ['brave','powerful','amazing'];
+let noun = ['dog','captain','Lego'];
+let tld = ['.com','.net','.org',];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// 2. Nested for loops to iterate through the arrays
+for(let i = 0; i < pronoun.length; i++) {
+  for(let x = 0; x < adj.length; x++) {
+    for(let y = 0; y < noun.length; y++) {
+      for(let z = 0; z < tld.length; z++) {
+        
+        // DOM manipulation
+        let domain = `${pronoun[i]}${adj[x]}${noun[y]}${tld[z]}`;
+
+        // Create a new paragraph element
+        let p = document.createElement("p");
+        p.textContent = domain;
+
+        // Append to the outputDiv
+        outputDiv.appendChild(p);
+      }
+    }
+  }
+}
+
+// 2. Nested for loops to iterate through the arrays
+// The loop but prints to console instead of the DOM
+// for(let i = 0; i < pronoun.length; i++) {
+//   for(let x = 0; x < adj.length; x++) {
+//     for(let y = 0; y < noun.length; y++) {
+//       for(let z = 0; z < tld.length; z++) {
+//         console.log(`${pronoun[i]}${adj[x]}${noun[y]}${tld[z]}`);
+//       }
+//     }
+//   }
+// }
